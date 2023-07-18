904f69f9006a75997c0f1dc54030987a84096854
vicn1708 <vicn1708@gmail.com>
2023-07-18 18:51:16 +0700

    remove update email

--- src/apis/user/dto/update-user.dto.ts
+++ src/apis/user/dto/update-user.dto.ts
@@ -2,5 +2,5 @@ import { OmitType, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(
-  OmitType(CreateUserDto, ['refreshToken']),
+  OmitType(CreateUserDto, ['refreshToken', 'email']),
 ) {}
